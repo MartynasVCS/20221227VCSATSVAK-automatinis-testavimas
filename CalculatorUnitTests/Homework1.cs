133849222f970891dcacc1b8ed39e11186f65719
MartynasVCS <martynasvcs@gmail.com>
2023-02-07 21:18:53 +0200

    Add solution to first exercise

---
+++ CalculatorUnitTests/Homework1.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+
+namespace CalculatorUnitTests
+{
+    internal class Homework1
+    {
+        [Test]
+        public void CheckRemainder995DividedBy3()
+        {
+            int expectedResult = 0;
+
+            int actualResult = 995 % 3;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
