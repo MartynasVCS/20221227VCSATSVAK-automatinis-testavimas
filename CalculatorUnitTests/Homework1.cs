e00143a5943d33fcf7b8f9ae61b4ca1537275cd2
MartynasVCS <martynasvcs@gmail.com>
2023-02-10 19:07:46 +0200

    Finish homework 1 code

--- CalculatorUnitTests/Homework1.cs
+++ CalculatorUnitTests/Homework1.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using System;
 
 namespace CalculatorUnitTests
 {
@@ -12,6 +13,57 @@ namespace CalculatorUnitTests
             int actualResult = 995 % 3;
 
             Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void CheckIfTodayIsWednesday()
+        {
+            DayOfWeek expectedResult = DayOfWeek.Wednesday;
+
+            DayOfWeek actualResult = DateTime.Now.DayOfWeek;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        [Test]
+        public void CheckIfTimeNowIs13h()
+        {
+            int expectedResult = 13;
+
+            int actualResult = DateTime.Now.Hour;
+
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+
+        /// <summary>
+        /// Pavyzdys kaip palyginti laiką įtraukiant ir minutes
+        /// Naudojantis šiuo pavyzdžiu galima "pasigaminti" bet kokį datos ir laiko formatą
+        /// </summary>
+        [Test]
+        public void CheckIfTimeNowIs1300h()
+        {
+            string expectedResult = "13:00";
+
+            string actualResult = $"{DateTime.Now.Hour}:{DateTime.Now.Minute}";
+            string actualResultFormatted = DateTime.Now.ToString("HH:mm");
+
+            Assert.AreEqual(expectedResult, actualResultFormatted);
+        }
+
+        [Test]
+        public void CheckIfThereAre4EvenNumbersBetween1And10()
+        {
+            int evenNumbersCount = 0;
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    evenNumbersCount++;
+                }
+            }
+
+            Assert.AreEqual(4, evenNumbersCount);
         }
     }
 }
