c29247b24317963496f1af306cbebb2116693b4a
MartynasVCS <martynasvcs@gmail.com>
2023-02-10 20:56:43 +0200

    Add test for Single Input Field in Selenium Demo website

---
+++ SeleniumTestsWithoutPOM/SeleniumEasy.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestsWithoutPOM
+{
+    public class SeleniumEasy
+    {
+        [Test]
+        public void SingleInputField()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://demo.seleniumeasy.com/basic-first-form-demo.html";
+
+            string expectedResult = "Labas";
+            IWebElement inputEnterMessage = driver.FindElement(By.XPath("//*[@id='get-input']//input"));
+            IWebElement buttonShowMessage = driver.FindElement(By.XPath("//*[@id='get-input']/button"));
+            IWebElement spanMessageText = driver.FindElement(By.XPath("//*[@id='display']"));
+
+            inputEnterMessage.SendKeys(expectedResult);
+            buttonShowMessage.Click();
+            string actualResult = spanMessageText.Text;
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+            driver.Quit();
+        }
+    }
+}
